b933f1c03fdfcb302af74c1f53819b7c7ca345f2
Wanchalerm Yuphasuk <enjizen.r@gmail.com>
2021-09-11 23:39:45 +0700

    jenkin

---
+++ src/test/kotlin/com/tua/wanchaelrm/customer/controller/ProfileControllerTest.kt
@@ -0,0 +1,20 @@
+package com.tua.wanchaelrm.customer.controller
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ProfileControllerTest {
+
+    @Test
+    fun addProfile() {
+    }
+
+    @Test
+    fun getProfile() {
+    }
+
+    @Test
+    fun getProfileWithEmail() {
+    }
+}
