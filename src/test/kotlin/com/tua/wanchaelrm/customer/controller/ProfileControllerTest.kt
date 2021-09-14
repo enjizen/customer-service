8dbada790cd47c418fd6b3eb5b3e36b114b53d25
Wanchalerm Yuphasuk <enjizen.r@gmail.com>
2021-09-14 11:43:15 +0700

    junit 5

--- src/test/kotlin/com/tua/wanchaelrm/customer/controller/ProfileControllerTest.kt
+++ src/test/kotlin/com/tua/wanchaelrm/customer/controller/ProfileControllerTest.kt
@@ -3,18 +3,18 @@ package com.tua.wanchaelrm.customer.controller
 import com.tua.wanchaelrm.customer.model.document.Profile
 import com.tua.wanchaelrm.customer.model.request.ProfileRequest
 import com.tua.wanchaelrm.customer.service.ProfileService
-import org.junit.Assert.assertEquals
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.*
-import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.junit.jupiter.MockitoExtension
 
 import org.springframework.http.HttpStatus
 
 
-@RunWith(MockitoJUnitRunner::class)
+@ExtendWith(MockitoExtension::class)
  class ProfileControllerTest {
 
     @InjectMocks
